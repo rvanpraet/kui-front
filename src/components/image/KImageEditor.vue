6081a02a4fc68e6ce64a78b63a74c04a8fe685fc
Hector Soria <attosoria@hotmail.com>
2020-11-02 23:13:39 +0100

    halfway to the image editor

--- src/components/image/KImageEditor.vue
+++ src/components/image/KImageEditor.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="resize-container" ref="resizeContainer">
-        <span class="resize-handle resize-handle-nw" @mousedown="startResize"></span>
-        <span class="resize-handle resize-handle-ne" @mousedown="startResize"></span>
-        <img class="resize-image" src="https://cdnuploads.aa.com.tr/uploads/Contents/2020/05/14/thumbs_b_c_88bedbc66bb57f0e884555e8250ae5f9.jpg?v=140708" ref="imageTarget" />
-        <span class="resize-handle resize-handle-se" @mousedown="startResize"></span>
-        <span class="resize-handle resize-handle-sw" @mousedown="startResize"></span>
+    <div class="canvas">
+        <div class="resize-container" ref="resizeContainer">
+            <span class="resize-handle resize-handle-nw" @mousedown="startResize"></span>
+            <span class="resize-handle resize-handle-ne" @mousedown="startResize"></span>
+            <img crossorigin="anonymous" class="resize-image" src="https://cdnuploads.aa.com.tr/uploads/Contents/2020/05/14/thumbs_b_c_88bedbc66bb57f0e884555e8250ae5f9.jpg?v=140708" ref="imageTarget" />
+            <span class="resize-handle resize-handle-sw" @mousedown="startResize"></span>
+            <span class="resize-handle resize-handle-se" @mousedown="startResize"></span>
+        </div>
     </div>
 </template>
 
@@ -20,10 +22,10 @@ export default {
             origSrc: new Image(),
             eventState: {},
             constrain: false,
-            minWidth: 60,
-            minHeight: 60,
-            maxWidth: 800,
-            maxHeight: 900,
+            minWidth: 6,
+            minHeight: 6,
+            maxWidth: 800000,
+            maxHeight: 900000,
             resizeCanvas: document.createElement('canvas'),
         };
     },
@@ -33,6 +35,7 @@ export default {
             // Create a new image with a copy of the original src
             // When resizing, we will always use this original copy as the base
             this.origSrc.src = this.$refs.imageTarget.src;
+            this.origSrc.setAttribute('crossorigin', 'anonymous');
         },
         startResize(e) {
             e.preventDefault();
@@ -49,17 +52,37 @@ export default {
             let height;
             let left;
             let top;
-            // let offset = {
-            //     top: this.$refs.resizeContainer.getBoundingClientRect().top + document.body.scrollTop,
-            //     left: this.$refs.resizeContainer.getBoundingClientRect().left + document.body.scrollLeft,
-            // };
-            mouse.x = (e.clientX || e.pageX || e.changedTouches[0].clientX) + window.scrollLeft || 0; // TODO
-            mouse.y = (e.clientY || e.pageY || e.changedTouches[0].clientY) + window.scrollTop || 0; // TODO
 
-            width = mouse.x - this.eventState.container_left;
-            height = mouse.y  - this.eventState.container_top;
-            left = this.eventState.container_left;
-            top = this.eventState.container_top;
+            mouse.x = (e.clientX || e.pageX || e.changedTouches[0].clientX) + window.scrollX; // TODO
+            mouse.y = (e.clientY || e.pageY || e.changedTouches[0].clientY) + window.scrollY; // TODO
+            // Position image differently depending on the corner dragged and constraints
+            if(this.eventState.evnt.target.classList.contains('resize-handle-se')){
+                width = mouse.x - this.eventState.container_left;
+                height = mouse.y  - this.eventState.container_top;
+                left = this.$refs.resizeContainer.offsetLeft;
+                top = this.$refs.resizeContainer.offsetTop;
+            } else if(this.eventState.evnt.target.classList.contains('resize-handle-sw')){
+                width = this.eventState.container_width - (mouse.x - this.eventState.container_left);
+                height = mouse.y  - this.eventState.container_top;
+                left = mouse.x;
+                top = this.$refs.resizeContainer.offsetTop;
+            } else if(this.eventState.evnt.target.classList.contains('resize-handle-nw')){
+                width = this.eventState.container_width - (mouse.x - this.eventState.container_left);
+                height = this.eventState.container_height - (mouse.y - this.eventState.container_top);
+                left = mouse.x;
+                top = mouse.y;
+                if(this.constrain || e.shiftKey){
+                    top = mouse.y - ((width / this.origSrc.width * this.origSrc.height) - height);
+                }
+            } else if(this.eventState.evnt.target.classList.contains('resize-handle-ne')){
+                width = mouse.x - this.eventState.container_left;
+                height = this.eventState.container_height - (mouse.y - this.eventState.container_top);
+                left = this.eventState.container_left;
+                top = mouse.y;
+                if(this.constrain || e.shiftKey){
+                    top = mouse.y - ((width / this.origSrc.width * this.origSrc.height) - height);
+                }
+            }
 
             if(this.constrain || e.shiftKey){
                 height = width / this.origSrc.width * this.origSrc.height;
@@ -67,14 +90,11 @@ export default {
             if(width > this.minWidth && height > this.minHeight && width < this.maxWidth && height < this.maxHeight) {
                 this.resizeImage(width, height);
                 // Without this Firefox will not re-calculate the the image dimensions until drag end
-                console.log(left);
-                console.log(top);
-                // this.$refs.resizeContainer.offset({'left': left, 'top': top}); // TODO
+                this.$refs.resizeContainer.style.top = top + "px";
+                this.$refs.resizeContainer.style.left = left + "px";
             }
         },
         resizeImage(width, height) {
-            console.log(width);
-            console.log(height);
             this.resizeCanvas.width = width;
             this.resizeCanvas.height = height;
             this.resizeCanvas.getContext('2d').drawImage(this.origSrc, 0, 0, width, height);
@@ -88,21 +108,25 @@ export default {
             document.removeEventListener('touchmove', this.resizing);
         },
         saveEventState(e) {
-            this.eventState.container_width = this.$refs.resizeContainer.width;
-            this.eventState.container_height = this.$refs.resizeContainer.height;
-            this.eventState.container_left = this.$refs.resizeContainer.offsetLeft;
-            this.eventState.container_top = this.$refs.resizeContainer.offsetTop;
-            this.eventState.mouse_x = (e.clientX || e.pageX || e.changedTouches[0].clientX) + window.scrollLeft || 0; // TODO
-            this.eventState.mouse_y = (e.clientY || e.pageY || e.changedTouches[0].clientY) + window.scrollTop || 0; // TODO
 
+            this.eventState.container_width = parseFloat(getComputedStyle(this.$refs.resizeContainer, null).width.replace("px", ""));
+            this.eventState.container_height = parseFloat(getComputedStyle(this.$refs.resizeContainer, null).height.replace("px", ""));
+
+
+            const rect = this.$refs.resizeContainer.getBoundingClientRect();
+            this.eventState.container_left = rect.left + window.scrollX;
+            this.eventState.container_top = rect.top + window.scrollY;
+
+            this.eventState.mouse_x = (e.clientX || e.pageX || e.changedTouches[0].clientX) + window.scrollX; // TODO
+            this.eventState.mouse_y = (e.clientY || e.pageY || e.changedTouches[0].clientY) + window.scrollY; // TODO
             // This is a fix for mobile safari
             // For some reason it does not allow a direct copy of the touches property
             if(typeof e.changedTouches !== 'undefined'){
                 this.eventState.touches = [];
                 e.changedTouches.forEach(function(i, ob){
                     this.eventState.touches[i] = {};
-                    this.eventState.touches[i].clientX = 0+ob.clientX;
-                    this.eventState.touches[i].clientY = 0+ob.clientY;
+                    this.eventState.touches[i].clientX = 0 + ob.clientX;
+                    this.eventState.touches[i].clientY = 0 + ob.clientY;
                 }.bind(this));
             }
             this.eventState.evnt = e;
@@ -117,6 +141,15 @@ export default {
 </script>
 
 <style lang="scss">
+    .canvas {
+        position: relative;
+        background-color: grey;
+        height: 500px;
+        border: 3px solid #49708A;
+        max-width: 901px;
+        overflow: hidden;
+        margin: 0 auto;
+    }
     /* Container images */
     .resize-container {
         position: relative;
